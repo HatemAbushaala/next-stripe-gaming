f77ae8527ca70a2cff091d8891ca6b4a900814b4
hatem abushaala <hatem.a.abushaala@gmail.com>
2023-09-27 17:50:53 +0300

    setup basic demo

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,56 @@
+'use client'
+
 import Image from 'next/image'
 import styles from './page.module.css'
+import Form from '@/components/Form'
+import Container from '@/components/Container'
+import axios from 'axios'
+import { useState } from 'react'
 
 export default function Home() {
+  const [clientSecret, setClientSecret] = useState()
+  const [cartId, setCartId] = useState()
+
+  const handleCheckout = async (e) => { 
+    e.preventDefault()
+    const email = e.target[0].value
+    if(!email)return alert('email required')
+    const order_response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/checkout`,{
+      "email": email,
+      "items": [
+          {
+              "quantity": 1,
+              "title": "custom",
+              "unit_price": 1500
+          }
+      ],
+    })
+
+    const _cart_id = order_response.data.medusa.draft_order.cart_id
+
+    const payment_session_response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/create-payment-session`,{
+      medusa_cart_id:_cart_id
+    })
+
+    const _client_secret = payment_session_response.data.cart.payment_session.data.client_secret
+    // console.log(payment_session_response.data)
+    // console.log('response ',payment_session_response.data.cart.payment_session.data.client_secret)
+    setCartId(_cart_id)
+    setClientSecret(_client_secret)
+   }
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main >
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      { !clientSecret && (
+        <form onSubmit={handleCheckout}>
+          <input required placeholder='email' type='email' name='email'/>
+        <button type='submit' >checkout</button>
+        </form>
+      )}
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      { clientSecret && cartId && (
+        <Container cartId={cartId} clientSecret={clientSecret}/>
+      )}
     </main>
   )
 }
